011386f4b2f13f352122cb0618d0afd9eff7a537
Alex Markessinis <markea125@gmail.com>
2017-11-27 11:34:51 -0500

    using conf path under etc

--- includes/helpers.py
+++ includes/helpers.py
@@ -8,7 +8,7 @@ import base64
 # read config.yaml
 def read_config():
     # Open the config file
-    with open('config/config.yaml') as data_file:
+    with open('/etc/orm/ldap/config.yaml') as data_file:
         # return the json object as a python object
         return load(data_file)
 
