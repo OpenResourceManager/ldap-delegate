4a2d502f4d14eda093fe3c32ecf4ab276e035210
Alex Markessinis <markea125@gmail.com>
2017-11-27 11:23:40 -0500

    no idea if this will work

--- LdapDelegate.py
+++ LdapDelegate.py
@@ -3,9 +3,12 @@ from __future__ import print_function
 from socketIO_client import SocketIO, LoggingNamespace
 from includes.AdManager import AdManager
 from includes.helpers import read_config, read_encrypted_message
-from includes.Daemon import Daemon
+from signal import SIGTERM
 import socket
 import sys
+import os
+import time
+import atexit
 
 IO = None
 HOST_NAME = None
@@ -14,9 +17,123 @@ DELEGATE_HOST = 'localhost'
 DELEGATE_PORT = 3000
 
 
-class LdapDelegate(Daemon):
-    @staticmethod
-    def load_config():
+class LdapDelegate:
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/var/log/orm/LdapDelegate/run.log',
+                 stderr='/var/log/orm/LdapDelegate/error.log'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile, 'w+').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return  # not an error in a restart
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print
+                str(err)
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def load_config(self):
         global HOST_NAME, DELEGATE_HOST, DELEGATE_PORT, BC_KEY
         c = read_config()
         config = c['general']
@@ -30,8 +147,7 @@ class LdapDelegate(Daemon):
             raise KeyError('You have not provided a `bc_key` in your config file! Hint: `php artisan orm:bckey`')
         BC_KEY = config['bc_key']
 
-    @staticmethod
-    def connect_to_sio():
+    def connect_to_sio(self):
         global IO
         print('Connecting...')
         IO = SocketIO(DELEGATE_HOST, DELEGATE_PORT, LoggingNamespace)
@@ -424,8 +540,7 @@ class LdapDelegate(Daemon):
 
 
 if __name__ == "__main__":
-    daemon = LdapDelegate('/var/run/ldap-delegate.pid', '/var/log/orm/LdapDelegate/run.log',
-                          '/var/log/orm/LdapDelegate/error.log')
+    daemon = LdapDelegate('/var/run/ldap-delegate.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
